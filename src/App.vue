04878e0a2b817f4b7621ba449c20c3702649f4dc
valart-platform <valart-platform@gmail.com>
2023-05-07 00:03:50 +0200

    v-for

--- src/App.vue
+++ src/App.vue
@@ -21,12 +21,26 @@
       body="Description 3"
       color-text="success"
     />
+    <BlogPost
+      v-for="post in posts"
+      :id="post.id"
+      :key="post.id"
+      :title="post.title"
+      :body="post.body"
+      :color-text="post.colorText"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import BlogPost from './components/BlogPost.vue'
 import ButtonCounter from './components/ButtonCounter.vue'
+const posts = ref([
+  { title: 'Post 1', id: '1', body: 'Description 1', colorText: 'primary' },
+  { title: 'Post 2', id: '2', body: 'Description 2', colorText: 'secondary' },
+  { title: 'Post 3', id: '3', body: 'Description 3', colorText: 'success' }
+])
 </script>
 
 <style scoped></style>
